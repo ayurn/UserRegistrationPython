87db2c3632f0fc074311325e5ddeb0b4ceb3b707
ayurn <ninaweayur@gmail.com>
2021-07-07 18:17:23 +0530

    [Ayur] Add : Added code for user last name validation and unit test for last name.

--- UnitTest/unitTestUserReg.py
+++ UnitTest/unitTestUserReg.py
@@ -15,3 +15,15 @@ class TestUserRegistration(unittest.TestCase):
         user = UserRegistration()
         result = user.first_name_regex(first_name)
         self.assertFalse(result)
+
+    def test_last_name_regex_givenCorrectName_shouldReturnTrue(self):
+        last_name = "Ninawe"
+        user = UserRegistration()
+        result = user.last_name_regex(last_name)
+        self.assertTrue(result)
+
+    def test_last_name_regex_givenIncorrectName_shouldReturnFalse(self):
+        last_name = "ninawe"
+        user = UserRegistration()
+        result = user.last_name_regex(last_name)
+        self.assertFalse(result)
--- UserRegistration.py
+++ UserRegistration.py
@@ -16,3 +16,16 @@ class UserRegistration :
             return bool(re.match(name_pattern, first_name))
         except Exception :
             loggerfile.Logger("debug", "Invalid Program")
+
+    def last_name_regex(self, last_name):
+        """
+           Description:
+               Function to validate last name.
+           Parameter:
+               last_name : get last name to validate
+           """
+        try:
+            name_pattern = r"^[A-Z]{1}[a-zA-Z]{2,}$"
+            return bool(re.match(name_pattern, last_name))
+        except Exception:
+            loggerfile.Logger("debug", "Invalid Program")
